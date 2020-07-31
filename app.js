c3a749476fc7855837cf1fa1aaac036304465a70
Rahul Kabra <rahulkabra029@gmail.com>
2020-07-31 17:31:05 +0530

    Removed jQuery dependency. Minor fixes and styling done.

--- app.js
+++ app.js
@@ -1,228 +1,236 @@
-let showCluesFlag = false;
-let themeFlag = false;
-let houseCount = 2;
-let beaubaxtonsCount = 2;
-let horcruxCount = 1;
-let creaturesCount = 2;
-let centaurCount = 2;
-let houseelfCount = 2;
-let ghostCount = 2;
-let teacherCount = 3;
-let deatheaterCount = 3;
-let orderCount = 3;
-let durmstrangCount = 3;
-let muggleCount = 4;
-let ministryCount = 12;
-let unknownCount = 11;
-let hufflepuffCount = 6;
-let ravenclawCount = 11;
-let slytherinCount = 15;
-let gryffindorCount = 27;
+var showCluesFlag = false;
+var themeFlag = false;
+var houseCount = 2;
+var beaubaxtonsCount = 2;
+var horcruxCount = 1;
+var creaturesCount = 2;
+var centaurCount = 2;
+var houseelfCount = 2;
+var ghostCount = 2;
+var teacherCount = 3;
+var deatheaterCount = 3;
+var orderCount = 3;
+var durmstrangCount = 3;
+var muggleCount = 4;
+var ministryCount = 12;
+var unknownCount = 11;
+var hufflepuffCount = 6;
+var ravenclawCount = 11;
+var slytherinCount = 15;
+var gryffindorCount = 27;
 
-$(document).ready(function () {
-   if ($("#userInput").val() !== "") {
-      $("#answer").attr("disabled", false);
-      $("#answer").removeClass("disabled");
-   }
-   else {
-      $("#answer").attr("disabled", true);
-      $("#answer").addClass("disabled");
-   }
-});
+window.addEventListener("load", checkVal);
 
 function checkVal() {
-   if ($("#userInput").val() !== "") {
-      $("#answer").attr("disabled", false);
-      $("#answer").removeClass("disabled");
+   if (document.getElementById('userInput').value !== '') {
+      document.getElementById("answer").disabled = false;
+      document.getElementById("answer").classList.remove("disabled");
    }
    else {
-      $("#answer").attr("disabled", true);
-      $("#answer").addClass("disabled");
+      document.getElementById("answer").disabled = true;
+      document.getElementById("answer").classList.add("disabled");
    }
 }
 
 function onAnswerSubmit() {
-   let answer = $("#userInput").val().toLowerCase();
-   $.grep(data, function (e, i) {
+   let answer = document.getElementById("userInput").value.toLowerCase();
+   data.filter(function (e) {
       if (e.characterName === answer) {
-         $("#" + answer).text(answer);
-         let parent = $("#" + answer).parent().parent()[0].id;
-         let grandParent = $("#" + parent).parent().parent()[0].id;
-         let className = $("#" + grandParent)[0].className.split(" ")[1];
-         $("#" + parent).addClass(className + "-colors");
+         document.getElementById(answer).innerText = answer;
+         let parent = document.getElementById(answer).parentNode.parentNode.id;
+         let grandParent = document.getElementById(parent).parentNode.parentNode.id;
+         let className = document.getElementById(grandParent).className.split(" ")[1];
+         document.getElementById(parent).classList.add(className + "-colors");
          countChecker(e);
-         $("#" + grandParent).attr("data-clue", e.clue);
-         $("#userInput").val("");
-         $("#userInput").focusout();
-         $("#answer").attr("disabled", true);
-         $("#answer").addClass("disabled");
+         document.getElementById(grandParent).setAttribute("data-clue", e.clue);
+         document.getElementById("userInput").value = "";
+         document.querySelector(".label-txt").classList.remove("label-active");
+         document.getElementById("answer").disabled = true;
+         document.getElementById("answer").classList.add("disabled");
       }
       else {
-         $("userInput").focus();
-         $("#answer").attr("disabled", false);
-         $("#answer").removeClass("disabled");
+         document.getElementById("answer").disabled = false;
+         document.getElementById("answer").classList.remove("disabled");
       }
-   })
+   });
 }
 
 function countChecker(e) {
    if (e.category === "house") {
-      houseCount--;
-      $("#house-answer-count").text(houseCount);
+      if (houseCount !== 0)
+         houseCount--;
+      document.getElementById("house-answer-count").innerText = houseCount;
       if (houseCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "beaubaxtons") {
-      beaubaxtonsCount--;
-      $("#beaubaxtons-answer-count").text(beaubaxtonsCount);
+      if (beaubaxtonsCount !== 0)
+         beaubaxtonsCount--;
+      document.getElementById("beaubaxtons-answer-count").innerText = beaubaxtonsCount;
       if (beaubaxtonsCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "horcrux") {
-      horcruxCount--;
-      $("#horcrux-answer-count").text(horcruxCount);
+      if (horcruxCount !== 0)
+         horcruxCount--;
+      document.getElementById("horcrux-answer-count").innerText = horcruxCount;
       if (horcruxCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "creatures") {
-      creaturesCount--;
-      $("#creatures-answer-count").text(creaturesCount);
+      if (creaturesCount !== 0)
+         creaturesCount--;
+      document.getElementById("creatures-answer-count").innerText = creaturesCount;
       if (creaturesCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "centaur") {
-      centaurCount--;
-      $("#centaur-answer-count").text(centaurCount);
+      if (centaurCount !== 0)
+         centaurCount--;
+      document.getElementById("centaur-answer-count").innerText = centaurCount;
       if (centaurCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "houseelf") {
-      houseelfCount--;
-      $("#houseelf-answer-count").text(houseelfCount);
+      if (houseelfCount !== 0)
+         houseelfCount--;
+      document.getElementById("houseelf-answer-count").innerText = houseelfCount;
       if (houseelfCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "ghost") {
-      ghostCount--;
-      $("#ghost-answer-count").text(ghostCount);
+      if (ghostCount !== 0)
+         ghostCount--;
+      document.getElementById("ghost-answer-count").innerText = ghostCount;
       if (ghostCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "teacher") {
-      teacherCount--
-      $("#teacher-answer-count").text(teacherCount);
+      if (teacherCount !== 0)
+         teacherCount--
+      document.getElementById("teacher-answer-count").innerText = teacherCount;
       if (teacherCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "death-eater") {
-      deatheaterCount--;
-      $("#death-eater-answer-count").text(deatheaterCount);
+      if (deatheaterCount !== 0)
+         deatheaterCount--;
+      document.getElementById("death-eater-answer-count").innerText = deatheaterCount;
       if (deatheaterCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "order") {
-      orderCount--;
-      $("#order-answer-count").text(orderCount);
+      if (orderCount !== 0)
+         orderCount--;
+      document.getElementById("order-answer-count").innerText = orderCount;
       if (orderCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "durmstrang") {
-      durmstrangCount--;
-      $("#durmstrang-answer-count").text(durmstrangCount);
+      if (durmstrangCount !== 0)
+         durmstrangCount--;
+      document.getElementById("durmstrang-answer-count").innerText = durmstrangCount;
       if (durmstrangCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "muggle") {
-      muggleCount--;
-      $("#muggle-answer-count").text(muggleCount);
+      if (muggleCount !== 0)
+         muggleCount--;
+      document.getElementById("muggle-answer-count").innerText = muggleCount;
       if (muggleCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "ministry") {
-      ministryCount--;
-      $("#ministry-answer-count").text(ministryCount);
+      if (ministryCount !== 0)
+         ministryCount--;
+      document.getElementById("ministry-answer-count").innerText = ministryCount;
       if (ministryCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "unknown") {
-      unknownCount--;
-      $("#unknown-answer-count").text(unknownCount);
+      if (unknownCount !== 0)
+         unknownCount--;
+      document.getElementById("unknown-answer-count").innerText = unknownCount;
       if (unknownCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "hufflepuff") {
-      hufflepuffCount--;
-      $("#hufflepuff-answer-count").text(hufflepuffCount);
+      if (hufflepuffCount !== 0)
+         hufflepuffCount--;
+      document.getElementById("hufflepuff-answer-count").innerText = hufflepuffCount;
       if (hufflepuffCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "ravenclaw") {
-      ravenclawCount--;
-      $("#ravenclaw-answer-count").text(ravenclawCount);
+      if (ravenclawCount !== 0)
+         ravenclawCount--;
+      document.getElementById("ravenclaw-answer-count").innerText = ravenclawCount;
       if (ravenclawCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "slytherin") {
-      slytherinCount--;
-      $("#slytherin-answer-count").text(slytherinCount);
+      if (slytherinCount !== 0)
+         slytherinCount--;
+      document.getElementById("slytherin-answer-count").innerText = slytherinCount;
       if (slytherinCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
    else if (e.category === "gryffindor") {
-      gryffindorCount--;
-      $("#gryffindor-answer-count").text(gryffindorCount);
+      if (gryffindorCount !== 0)
+         gryffindorCount--;
+      document.getElementById("gryffindor-answer-count").innerText = gryffindorCount;
       if (gryffindorCount === 0)
-         $("#" + e.category + "-count_content").addClass(e.category + "-colors")
+         document.getElementById(e.category + "-count_content").classList.add(e.category + "-colors")
    }
 }
 
-$('#about').click(function () {
-   $('#overlay').addClass('open');
+document.getElementById("about").addEventListener("click", function () {
+   document.getElementById("overlay").classList.add("open");
 });
 
-$('#userInput').focus(function () {
-   $(this).parent().find(".label-txt").addClass('label-active');
+function closeModal() {
+   document.getElementById("overlay").classList.remove("open");
+}
+
+document.getElementById('userInput').addEventListener("focusin", function () {
+   document.querySelector(".label-txt").classList.add("label-active");
 });
 
-$("#userInput").focusout(function () {
-   if ($(this).val() == '') {
-      $(this).parent().find(".label-txt").removeClass('label-active');
+document.getElementById('userInput').addEventListener("focusout", function () {
+   if (document.getElementById('userInput').value === '') {
+      document.querySelector(".label-txt").classList.remove("label-active");
    };
 });
 
 function toggleThemeMode() {
-   if ($("html")[0].dataset.theme == "light") {
-      $("#mode").removeClass("fa-sun-o");
-      $("#mode").addClass("fa-moon-o");
-      $("html")[0].dataset.theme = "dark";
+   if (document.querySelector("html").getAttribute("data-theme") == "light") {
+      document.getElementById("mode").classList.remove("fa-sun-o");
+      document.getElementById("mode").classList.add("fa-moon-o");
+      document.querySelector("html").setAttribute("data-theme", "dark");
    }
-   else if ($("html")[0].dataset.theme == "dark") {
-      $("#mode").removeClass("fa-moon-o");
-      $("#mode").addClass("fa-sun-o");
-      $("html")[0].dataset.theme = "light";
+   else if (document.querySelector("html").getAttribute("data-theme") == "dark") {
+      document.getElementById("mode").classList.remove("fa-moon-o");
+      document.getElementById("mode").classList.add("fa-sun-o");
+      document.querySelector("html").setAttribute("data-theme", "light");
    }
-}
-
-function closeModal() {
-   $('#overlay').removeClass('open');
 }
 
 function showClues() {
    showCluesFlag = !showCluesFlag;
    if (showCluesFlag) {
-      $("#clues").removeClass("fa-lock");
-      $("#clues").addClass("fa-unlock-alt");
+      document.getElementById("clues").classList.remove("fa-lock");
+      document.getElementById("clues").classList.add("fa-unlock-alt");
    }
    else {
-      $("#clues").removeClass("fa-unlock-alt");
-      $("#clues").addClass("fa-lock");
+      document.getElementById("clues").classList.remove("fa-unlock-alt");
+      document.getElementById("clues").classList.add("fa-lock");
    }
-   $.grep(data, function (e, i) {
+   data.filter(function (e) {
       if (showCluesFlag) {
-         $("#element_" + e.characterNumber).attr("data-clue", e.clue);
+         document.getElementById("element_" + e.characterNumber).setAttribute("data-clue", e.clue);
       }
       else {
-         $("#element_" + e.characterNumber).attr("data-clue", "");
+         document.getElementById("element_" + e.characterNumber).setAttribute("data-clue", "");
       }
-   })
+   });
 }
